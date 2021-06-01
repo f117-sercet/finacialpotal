e35079a3b1045b13befa760f82ce1a6fdb47470d
f117-serect <15159885771@163.com>
2021-06-01 15:28:08 +0800

    service-common 工具类

---
+++ srm/service-common/src/main/java/com/epdemic/common/util/Md5.java
@@ -0,0 +1,38 @@
+package com.epdemic.common.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * 加密规则
+ * @Author:estic
+ * @Date: 2021/6/1 15:04
+ */
+public class Md5 {
+
+    public static String encrypt(String strSrc) {
+        try {
+            char[] hexChars = { '0', '1', '2', '3', '4', '5', '6', '7', '8',
+                    '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+            byte[] bytes = strSrc.getBytes();
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(bytes);
+            bytes = md.digest();
+            int j = bytes.length;
+            char[] chars = new char[j * 2];
+            int k = 0;
+            for (int i = 0; i < bytes.length; i++) {
+                byte b = bytes[i];
+                chars[k++] = hexChars[b >>> 4 & 0xf];
+                chars[k++] = hexChars[b & 0xf];
+            }
+            return new String(chars);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            throw new RuntimeException("MD5加密出错！！+" + e);
+        }
+    }
+
+
+}
+
