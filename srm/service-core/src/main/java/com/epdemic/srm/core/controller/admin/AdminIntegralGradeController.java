63d37c5991d4a56d30f85d27e33a3336d7b95a20
f117-serect <15159885771@163.com>
2021-06-02 11:13:40 +0800

    前端部分更新

--- srm/service-core/src/main/java/com/epdemic/srm/core/controller/admin/AdminIntegralGradeController.java
+++ srm/service-core/src/main/java/com/epdemic/srm/core/controller/admin/AdminIntegralGradeController.java
@@ -1,7 +1,10 @@
 package com.epdemic.srm.core.controller.admin;
 
+import com.epdemic.common.result.R;
 import com.epdemic.srm.core.pojo.entity.IntegralGrade;
 import com.epdemic.srm.core.service.IntegralGradeService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -20,13 +23,22 @@ public class AdminIntegralGradeController {
     @Resource
     private IntegralGradeService integralGradeService;
 
+    @ApiOperation("积分等级列表")
     @GetMapping("/list")
-    public List<IntegralGrade> listAll(){
-        return integralGradeService.list();
+    public R listAll(){
+        List<IntegralGrade> list = integralGradeService.list();
+        return R.ok().data("list",list).message("获取列表成功");
     }
+    @ApiOperation(value = "根据id删除数据记录", notes="逻辑删除数据记录")
     @DeleteMapping("/remove/{id}")
-    public boolean removeById(@PathVariable Long id){
-
-        return integralGradeService.removeById(id);
+    public R removeById(
+            @ApiParam(value = "数据id", example = "100", required = true)
+            @PathVariable Long id){
+        boolean result = integralGradeService.removeById(id);
+        if(result){
+            return R.ok().message("删除成功");
+        }else{
+            return R.error().message("删除失败");
+        }
     }
 }
