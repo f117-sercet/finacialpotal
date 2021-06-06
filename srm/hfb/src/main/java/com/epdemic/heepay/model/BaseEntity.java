4c84d23b63b70432e77a9ce2cd70a092850171fa
f117-serect <15159885771@163.com>
2021-06-06 10:25:43 +0800

    汇付宝实体类构建

---
+++ srm/hfb/src/main/java/com/epdemic/heepay/model/BaseEntity.java
@@ -0,0 +1,34 @@
+package com.epdemic.heepay.model;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**基本实体
+ * @Author:estic
+ * @Date: 2021/6/6 10:24
+ */
+@Data
+public class BaseEntity  implements Serializable {
+
+    @ApiModelProperty(value = "id")
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    @ApiModelProperty(value = "创建时间")
+    @TableField("create_time")
+    private Date createTime;
+
+    @ApiModelProperty(value = "更新时间")
+    @TableField("update_time")
+    private Date updateTime;
+
+    @ApiModelProperty(value = "逻辑删除(1:已删除，0:未删除)")
+    @TableField("is_deleted")
+    private Integer isDeleted;
+}
