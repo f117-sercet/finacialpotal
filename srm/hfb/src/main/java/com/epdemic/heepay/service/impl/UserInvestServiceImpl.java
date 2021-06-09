7813f96045e48a5cfe578979c9819f854ca7d3cc
f117-serect <15159885771@163.com>
2021-06-09 17:00:15 +0800

    hfbController

--- srm/hfb/src/main/java/com/epdemic/heepay/service/impl/UserInvestServiceImpl.java
+++ srm/hfb/src/main/java/com/epdemic/heepay/service/impl/UserInvestServiceImpl.java
@@ -19,6 +19,7 @@ import org.thymeleaf.util.StringUtils;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -57,19 +58,96 @@ public class UserInvestServiceImpl extends ServiceImpl<UserInvestMapper, UserInv
         voteAmTotal = StringUtils.isEmpty(voteAmTotal)?"0":voteAmTotal;
         String curVoteAmtTotal = new BigDecimal(voteAmTotal).add(new BigDecimal(userInvest.getVoteAmt())).toString();
 
-
-        return null;
+        if(Double.parseDouble(curVoteAmtTotal) > Double.parseDouble(userInvest.getProjectAmt())) {
+            throw new HfbException(ResultCodeEnum.INVEST_AMMOUNT_MORE_ERROR);
+        }
+        UserInvest userInvest1 = userInvestMapper.selectByAgentProjectCode(userInvest.getAgentProjectCode());
+        if(null != userInvest1 && Double.parseDouble(userInvest.getProjectAmt()) != Double.parseDouble(userInvest1.getProjectAmt())) {
+            throw new HfbException(ResultCodeEnum.PROJECT_AMMOUNT_ERROR);
+        }
+        this.save(userInvest);
+        userInvest.setResultCode("0001");
+        userInvest.setResultMsg("成功");
+        userAccountService.lockAccount(userInvest.getVoteBindCode(),userInvest.getVoteAmt());
+        return userInvest;
 
 
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public String makeLoan(Map<String, Object> paramMap) {
-        return null;
-    }
+
+        String agentProjectCode = (String) paramMap.get("agentProjectCode");
+
+        // 项目投资的总金额
+        String voteAmtTotal = userInvestMapper.selectSumVoteAmtByAgentProjectCode(agentProjectCode);
+
+        //确保接口调用的幂等性。检验投资信息的状态，如果状态已经修改，则直接返回，不进行后续的转账操作
+        Integer status = userInvestMapper.selectStatusByAgentProjectCode(agentProjectCode);
+        if (status != null && status.intValue() == 1) {
+            return voteAmtTotal;
+        }
+
+        // 借款人绑定协议账号
+        String benefitBindCdoe = "";
+        List<UserInvest> userInvestList = userInvestMapper.selectList(new QueryWrapper<UserInvest>().eq("agent_project_code", agentProjectCode));
+
+        for (UserInvest userInvest : userInvestList) {
+            // 投资人绑定协议号
+            String voteBindCode = userInvest.getVoteBindCode();
+            // 借款人绑定协议号
+            benefitBindCdoe = userInvest.getBenefitBindCode();
+            //投资金额
+            String voteAmt = userInvest.getVoteAmt();
+            //解锁投资人金额
+            userAccountService.unLockAccount(voteBindCode, voteAmt);
+        }
+
+            //给借款人转账：
+            //从paramMap中获取平台服务费，然后从转账金额中减去平台的服务费
+            BigDecimal mchFee = new BigDecimal((String) paramMap.get("mchFee"));
+            BigDecimal total = new BigDecimal(voteAmtTotal).subtract(mchFee);
+            userAccountService.transferAccounts(benefitBindCdoe, total.toString());
+
+            //更新状态
+            UserInvest uptUserInvest = new UserInvest();
+            uptUserInvest.setStatus(1);
+            userInvestMapper.update(uptUserInvest, new QueryWrapper<UserInvest>().eq("agent_project_code", agentProjectCode));
+
+            return total.toString();
+
+        }
+
 
     @Override
     public String cancelLend(Map<String, Object> paramMap) {
-        return null;
+        String agentProjectCode = (String) paramMap.get("agentProjectCode");
+
+        // 项目投资的总金额
+        String voteAmtTotal = userInvestMapper.
+                selectSumVoteAmtByAgentProjectCode(agentProjectCode);
+        Integer status = userInvestMapper.selectStatusByAgentProjectCode(agentProjectCode);
+
+        if (null != status && status.intValue() == -1) {
+            return voteAmtTotal;
+        }
+        // 借款人绑定协议账号
+        String benefitBindCode = "";
+        List<UserInvest> userInvestList = userInvestMapper
+                .selectList(new QueryWrapper<UserInvest>().eq("agent_project_code", agentProjectCode));
+        for(UserInvest userInvest : userInvestList) {
+            //投资人绑定协议号
+            String voteBindCode = userInvest.getVoteBindCode();
+            //借款人绑定协议号
+            benefitBindCode = userInvest.getBenefitBindCode();
+            //投资金额
+            String voteAmt = userInvest.getVoteAmt();
+
+            //返回投资人金额
+            userAccountService.dealAccount(voteBindCode, voteAmt, "-"+voteAmt);
+        }
+
+        return voteAmtTotal;
     }
 }
