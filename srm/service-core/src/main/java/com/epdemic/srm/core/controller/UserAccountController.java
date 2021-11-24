2a9e1901502fd3a820cb1d542a52d8f6884269db
60221 <15159885771@163.com>
2021-11-24 15:58:39 +0800

    修改类

---
+++ srm/service-core/src/main/java/com/epdemic/srm/core/controller/UserAccountController.java
@@ -0,0 +1,21 @@
+package com.epdemic.srm.core.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 用户账户 前端控制器
+ * </p>
+ *
+ * @author combine
+ * @since 2021-11-24
+ */
+@RestController
+@RequestMapping("/userAccount")
+public class UserAccountController {
+
+}
+
