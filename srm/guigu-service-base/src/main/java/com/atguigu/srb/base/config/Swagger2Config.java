f515bea366adc257200d3498d982efe6fcee3cad
60221 <15159885771@163.com>
2021-11-28 16:54:04 +0800

    service-common接口调试

---
+++ srm/guigu-service-base/src/main/java/com/atguigu/srb/base/config/Swagger2Config.java
@@ -0,0 +1,44 @@
+package com.atguigu.srb.base.config;
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author estic
+ * swagger2配置
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+
+    @Bean
+    public Docket adminApiConfig(){
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("adminApi")
+                .apiInfo(adminApiInfo())
+                .select()
+                //只显示admin路径下的页面
+                .paths(Predicates.and(PathSelectors.regex("/admin/.*")))
+                .build();
+
+    }
+
+    private ApiInfo adminApiInfo(){
+
+        return new ApiInfoBuilder()
+                .title("尚融宝后台管理系统-API文档")
+                .description("本文档描述了尚融宝后台管理系统接口")
+                .version("1.0")
+                .contact(new Contact("dsc", null,"602218901@qq.com"))
+                .build();
+    }
+}
--- srm/guigu-service-core/src/main/java/com/atguigu/srb/core/controller/admin/AdminIntegralGradeController.java
+++ srm/guigu-service-core/src/main/java/com/atguigu/srb/core/controller/admin/AdminIntegralGradeController.java
@@ -2,6 +2,9 @@ package com.atguigu.srb.core.controller.admin;
 
 import com.atguigu.srb.core.pojo.entity.IntegralGrade;
 import com.atguigu.srb.core.service.IntegralGradeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -11,6 +14,7 @@ import java.util.List;
  * @author dsc
  *
  */
+@Api(tags = "积分等级管理")
 @CrossOrigin
 @RestController
 @RequestMapping("/admin/core/integralGrade")
@@ -18,11 +22,13 @@ public class AdminIntegralGradeController {
 
     @Resource
     private IntegralGradeService integralGradeService;
-
+   @ApiOperation("获取积分等级列表")
     @GetMapping("/list")
     public List<IntegralGrade> listAll(){
         return integralGradeService.list();
     }
+    @ApiOperation(value = "根据id删除积分等级", notes = "逻辑删除")
+    @ApiParam(value = "数据id", required = true, example = "1")
     @DeleteMapping("/remove/{id}")
     public boolean removeById(@PathVariable Long id){
         return integralGradeService.removeById(id);
