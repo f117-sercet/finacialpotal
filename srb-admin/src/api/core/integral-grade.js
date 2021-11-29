ffaccccfd974dc9eab1592dbdacf9e4a8ea8b51b
60221 <15159885771@163.com>
2021-11-29 17:17:38 +0800

    前台修改

--- srb-admin/src/api/core/integral-grade.js
+++ srb-admin/src/api/core/integral-grade.js
@@ -7,5 +7,34 @@ export default {
       url: '/admin/core/integralGrade/list',
       method: 'get'
     })
+  },
+  removeById(id) {
+    return request({
+      url: '/admin/core/integralGrade/remove/' + id,
+      method: 'delete'
+    })
+  },
+
+  save(integralGrade) {
+    return request({
+      url: '/admin/core/integralGrade/save',
+      method: 'post',
+      data: integralGrade
+    })
+  },
+
+  getById(id) {
+    return request({
+      url: '/admin/core/integralGrade/get/' + id,
+      method: 'get'
+    })
+  },
+
+  updateById(integralGrade) {
+    return request({
+      url: '/admin/core/integralGrade/update',
+      method: 'put',
+      data: integralGrade
+    })
   }
 }
