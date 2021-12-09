088f9c2909666dd704ae7560aadecf6322b46238
60221 <15159885771@163.com>
2021-12-09 15:07:39 +0800

    数据字典 redis

--- srm/guigu-service-core/src/main/java/com/atguigu/srb/core/controller/DictController.java
+++ srm/guigu-service-core/src/main/java/com/atguigu/srb/core/controller/DictController.java
@@ -1,9 +1,19 @@
 package com.atguigu.srb.core.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.atguigu.common.exception.BusinessException;
+import com.atguigu.common.result.R;
+import com.atguigu.common.result.ResponseEnum;
+import com.atguigu.srb.core.service.DictService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
+import java.io.InputStream;
 
 /**
  * <p>
@@ -13,9 +23,32 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Helen
  * @since 2021-11-25
  */
+@Api(tags = "数据字典管理")
 @RestController
-@RequestMapping("/dict")
+@RequestMapping("/admin/core/dict")
+@Slf4j
+@CrossOrigin
 public class DictController {
 
+    @Resource
+    private DictService dictService;
+
+    @ApiOperation("Excel批量导入数据字典")
+    @PostMapping("/import")
+    public R batchImport(
+            @ApiParam(value = "Excel文件", required = true)
+            @RequestParam("file") MultipartFile file) {
+
+        try {
+            InputStream inputStream = file.getInputStream();
+            dictService.importData(inputStream);
+            return R.ok().message("批量导入成功");
+        } catch (Exception e) {
+            //UPLOAD_ERROR(-103, "文件上传错误"),
+            throw new BusinessException(ResponseEnum.UPLOAD_ERROR, e);
+        }
+    }
 }
+
+
 
