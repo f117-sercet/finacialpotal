e9d9a3832937249bb06a5eb354a3b725e98584f8
suki <15159885771@163.com>
2022-02-03 23:42:28 +0800

    springcloud远程调用

--- srm/guigu_service_sms/src/main/java/com/ServiceSmsApplication.java
+++ srm/guigu_service_sms/src/main/java/com/ServiceSmsApplication.java
@@ -2,11 +2,13 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  *服务启动类
  */
+@EnableFeignClients
 @SpringBootApplication
 @ComponentScan({"com.atguigu.srb", "com.atguigu.common"})
 public class ServiceSmsApplication {
