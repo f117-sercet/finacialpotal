ab9fde8d04bde945400aaa7bc82ea62fbc0a7699
suki <15159885771@163.com>
2022-02-12 23:35:52 +0800

    汇付宝接口开发

---
+++ srm/atguigu_hfb/src/main/java/com/heepy/ManagerApplication.java
@@ -0,0 +1,13 @@
+package com.heepy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerApplication.class, args);
+    }
+
+}
