f25c35b0bf36c229686d4735d4f4b5d5a283532a
suki <15159885771@163.com>
2022-02-24 23:19:48 +0800

    汇付宝接口服务类

---
+++ srm/atguigu_hfb/src/main/java/com/heepy/service/LendReturnService.java
@@ -0,0 +1,11 @@
+package com.heepy.service;
+
+import java.util.Map;
+
+/**
+ * @author 段世超
+ */
+public interface LendReturnService {
+
+    Map<String,Object> returnCommit(Map<String,Object> paramMap);
+}
