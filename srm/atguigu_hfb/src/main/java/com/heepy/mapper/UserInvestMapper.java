cb54ddae206a3a336b97b00293a8c4991e27b99e
suki <15159885771@163.com>
2022-03-03 18:31:40 +0800

    汇付宝服务接口

--- srm/atguigu_hfb/src/main/java/com/heepy/mapper/UserInvestMapper.java
+++ srm/atguigu_hfb/src/main/java/com/heepy/mapper/UserInvestMapper.java
@@ -13,5 +13,14 @@ public interface UserInvestMapper extends BaseMapper<UserInvest> {
 
  IPage<UserInvest> selectPage(Page<UserInvest> page, @Param("vo")UserInvestQueryVo userInvestQueryVo);
 
+ /**
+  * 根据项目编号获取投资总金额
+  * @param agentProjectCode
+  * @return
+  */
  String selectSumVoteAmtByAgentProjectCode(@Param("agentProjectCode")String agentProjectCode);
+
+ UserInvest selectByAgentProjectCode(@Param("agentProjectCode") String agentProjectCode);
+
+ Integer selectStatusByAgentProjectCode(@Param("agentProjectCode") String agentProjectCode);
 }
