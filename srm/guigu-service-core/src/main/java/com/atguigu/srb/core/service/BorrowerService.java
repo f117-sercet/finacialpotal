33cb08b5cb2a29965d123bb392b36aa5f93c3d87
suki <15159885771@163.com>
2022-03-24 19:06:26 +0800

    汇付宝页面

--- srm/guigu-service-core/src/main/java/com/atguigu/srb/core/service/BorrowerService.java
+++ srm/guigu-service-core/src/main/java/com/atguigu/srb/core/service/BorrowerService.java
@@ -1,6 +1,11 @@
 package com.atguigu.srb.core.service;
 
+import com.atguigu.srb.core.pojo.Vo.BorrowerApprovalVO;
+import com.atguigu.srb.core.pojo.Vo.BorrowerDetailVO;
+import com.atguigu.srb.core.pojo.Vo.BorrowerVO;
 import com.atguigu.srb.core.pojo.entity.Borrower;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
@@ -12,5 +17,16 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2021-11-25
  */
 public interface BorrowerService extends IService<Borrower> {
+
+    void saveBorrowerVOByUserId(BorrowerVO borrowerVO, Long userId);
+
+    Integer getStatusByUserId(Long userId);
+
+    IPage<Borrower> listPage(Page<Borrower> pageParam, String keyword);
+
+    BorrowerDetailVO getBorrowerDetailVOById(Long id);
+
+    void approval(BorrowerApprovalVO borrowerApprovalVO);
+
 
 }
