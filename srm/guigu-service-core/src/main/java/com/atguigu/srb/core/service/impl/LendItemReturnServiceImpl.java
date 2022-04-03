77ecbd17ac7e03e2c213c6b3334e07fb78b2fc33
suki <15159885771@163.com>
2022-04-03 23:37:23 +0800

    selectByid

--- srm/guigu-service-core/src/main/java/com/atguigu/srb/core/service/impl/LendItemReturnServiceImpl.java
+++ srm/guigu-service-core/src/main/java/com/atguigu/srb/core/service/impl/LendItemReturnServiceImpl.java
@@ -1,10 +1,19 @@
 package com.atguigu.srb.core.service.impl;
 
+import com.atguigu.srb.core.mapper.LendItemMapper;
+import com.atguigu.srb.core.mapper.LendMapper;
+import com.atguigu.srb.core.mapper.LendReturnMapper;
 import com.atguigu.srb.core.pojo.entity.LendItemReturn;
 import com.atguigu.srb.core.mapper.LendItemReturnMapper;
 import com.atguigu.srb.core.service.LendItemReturnService;
+import com.atguigu.srb.core.service.UserBindService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -17,4 +26,39 @@ import org.springframework.stereotype.Service;
 @Service
 public class LendItemReturnServiceImpl extends ServiceImpl<LendItemReturnMapper, LendItemReturn> implements LendItemReturnService {
 
+    @Resource
+    private LendMapper lendMapper;
+
+    @Resource
+    private LendReturnMapper lendReturnMapper;
+
+    @Resource
+    private LendItemMapper lendItemMapper;
+
+    @Resource
+    private UserBindService userBindService;
+
+
+
+
+    @Override
+    public List<LendItemReturn> selectByLendId(Long lendId, Long userId) {
+
+        QueryWrapper<LendItemReturn> queryWrapper = new QueryWrapper<>();
+        queryWrapper
+                .eq("lend_id",lendId)
+                .eq("invest_user_id",userId)
+                .orderByAsc("current_period");
+        return baseMapper.selectList(queryWrapper);
+    }
+
+    @Override
+    public List<Map<String, Object>> addReturnDetail(Long lendReturnId) {
+        return null;
+    }
+
+    @Override
+    public List<LendItemReturn> selectLendItemReturnList(Long lendReturnId) {
+        return null;
+    }
 }
