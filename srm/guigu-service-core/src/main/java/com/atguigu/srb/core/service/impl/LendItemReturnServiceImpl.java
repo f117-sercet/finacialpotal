6eb362f530303f9aff1a5b2604bce6e6c0613b40
suki <15159885771@163.com>
2022-04-04 16:51:42 +0800

    还款id

--- srm/guigu-service-core/src/main/java/com/atguigu/srb/core/service/impl/LendItemReturnServiceImpl.java
+++ srm/guigu-service-core/src/main/java/com/atguigu/srb/core/service/impl/LendItemReturnServiceImpl.java
@@ -3,8 +3,11 @@ package com.atguigu.srb.core.service.impl;
 import com.atguigu.srb.core.mapper.LendItemMapper;
 import com.atguigu.srb.core.mapper.LendMapper;
 import com.atguigu.srb.core.mapper.LendReturnMapper;
+import com.atguigu.srb.core.pojo.entity.Lend;
+import com.atguigu.srb.core.pojo.entity.LendItem;
 import com.atguigu.srb.core.pojo.entity.LendItemReturn;
 import com.atguigu.srb.core.mapper.LendItemReturnMapper;
+import com.atguigu.srb.core.pojo.entity.LendReturn;
 import com.atguigu.srb.core.service.LendItemReturnService;
 import com.atguigu.srb.core.service.UserBindService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -12,6 +15,9 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -39,8 +45,6 @@ public class LendItemReturnServiceImpl extends ServiceImpl<LendItemReturnMapper,
     private UserBindService userBindService;
 
 
-
-
     @Override
     public List<LendItemReturn> selectByLendId(Long lendId, Long userId) {
 
@@ -52,9 +56,44 @@ public class LendItemReturnServiceImpl extends ServiceImpl<LendItemReturnMapper,
         return baseMapper.selectList(queryWrapper);
     }
 
+    /**
+     * 通过还款计划的id，找到对应的回款计划数据，组装data参数中需要的List<Map>
+     * @param lendReturnId
+     * @return
+     */
     @Override
     public List<Map<String, Object>> addReturnDetail(Long lendReturnId) {
-        return null;
+        //还款记录
+        LendReturn lendReturn = lendReturnMapper.selectById(lendReturnId);
+        //获取标的
+        Lend lend = lendMapper.selectById(lendReturn.getLendId());
+
+
+        List<LendItemReturn> lendItemReturnList = this.selectLendItemReturnList(lendReturnId);
+        List<Map<String, Object>> lendItemReturnDetailList = new ArrayList<>();
+        for (LendItemReturn lendItemReturn : lendItemReturnList) {
+
+            //获取投资记录
+            Long lendItemId = lendItemReturn.getLendItemId();
+            LendItem lendItem = lendItemMapper.selectById(lendItemId);
+
+            //获取投资人id
+            Long investUserId = lendItem.getInvestUserId();
+            String bindCode = userBindService.getBindCodeByUserId(investUserId);
+
+            Map<String, Object> map = new HashMap<>();
+            map.put("agentProjectCode", lend.getLendNo());//项目编号
+            map.put("voteBillNo", lendItem.getLendItemNo());//投资编号
+            map.put("toBindCode", bindCode); //投资人bindCode
+            map.put("transitAmt", lendItemReturn.getTotal());//还款总额
+            map.put("baseAmt", lendItemReturn.getPrincipal());//本金
+            map.put("benifitAmt", lendItemReturn.getInterest());//利息
+            map.put("feeAmt", new BigDecimal(0));
+
+            lendItemReturnDetailList.add(map);
+        }
+
+        return lendItemReturnDetailList;
     }
 
     @Override
