ead5aa8351899904319b6e95c4bffa6814e64663
suki <15159885771@163.com>
2022-04-11 10:27:11 +0800

    用户基本服务

--- srm/guigu-service-core/src/main/java/com/atguigu/srb/core/service/impl/UserInfoServiceImpl.java
+++ srm/guigu-service-core/src/main/java/com/atguigu/srb/core/service/impl/UserInfoServiceImpl.java
@@ -3,7 +3,9 @@ package com.atguigu.srb.core.service.impl;
 import com.atguigu.common.exception.Assert;
 import com.atguigu.common.result.ResponseEnum;
 import com.atguigu.common.util.MD5;
+import com.atguigu.srb.base.util.JwtUtils;
 import com.atguigu.srb.core.mapper.UserAccountMapper;
+import com.atguigu.srb.core.mapper.UserLoginRecordMapper;
 import com.atguigu.srb.core.pojo.Vo.LoginVO;
 import com.atguigu.srb.core.pojo.Vo.RegisterVO;
 import com.atguigu.srb.core.pojo.Vo.UserIndexVO;
@@ -11,6 +13,7 @@ import com.atguigu.srb.core.pojo.Vo.UserInfoVO;
 import com.atguigu.srb.core.pojo.entity.UserAccount;
 import com.atguigu.srb.core.pojo.entity.UserInfo;
 import com.atguigu.srb.core.mapper.UserInfoMapper;
+import com.atguigu.srb.core.pojo.entity.UserLoginRecord;
 import com.atguigu.srb.core.pojo.query.UserInfoQuery;
 import com.atguigu.srb.core.service.UserInfoService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -36,10 +39,49 @@ public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo> i
     @Resource
     private UserAccountMapper userAccountMapper;
 
+    @Resource
+    private UserLoginRecordMapper userLoginRecordMapper;
 
+    @Transactional(rollbackFor = Exception.class)
     @Override
     public UserInfoVO login(LoginVO loginVO, String ip) {
-        return null;
+
+        String mobile = loginVO.getMobile();
+        String password = loginVO.getPassword();
+        Integer userType = loginVO.getUserType();
+
+        //用户是否存在
+        QueryWrapper<UserInfo> userInfoQueryWrapper = new QueryWrapper<>();
+        userInfoQueryWrapper.eq("mobile",mobile)
+                .eq("user_type",userType);
+        UserInfo userInfo = baseMapper.selectOne(userInfoQueryWrapper);
+        Assert.notNull(userInfo,ResponseEnum.LOGIN_MOBILE_ERROR);
+
+        //密码是否正确
+        Assert.equals(MD5.encrypt(password),userInfo.getPassword(),ResponseEnum.LOGIN_PASSWORD_ERROR);
+
+        //用户是否被禁用
+        Assert.equals(userInfo.getStatus(),UserInfo.STATUS_NORMAL,ResponseEnum.LOGIN_LOKED_ERROR);
+
+
+        //记录登录日志
+        UserLoginRecord userLoginRecord = new UserLoginRecord();
+        userLoginRecord.setUserId(userInfo.getId());
+        userLoginRecord.setIp(ip);
+        userLoginRecordMapper.insert(userLoginRecord);
+
+        //生成token
+        String token = JwtUtils.createToken(userInfo.getId(), userInfo.getName());
+
+        //组装UserInfoVo
+        UserInfoVO userInfoVO = new UserInfoVO();
+        userInfoVO.setToken(token);
+        userInfoVO.setName(userInfo.getName());
+        userInfoVO.setNickName(userInfo.getNickName());
+        userInfoVO.setHeadImg(userInfo.getHeadImg());
+        userInfoVO.setMobile(mobile);
+        userInfoVO.setUserType(userType);
+        return userInfoVO;
     }
 
     @Override
@@ -49,6 +91,11 @@ public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo> i
 
     @Override
     public void lock(Long id, Integer status) {
+
+        UserInfo userInfo = new UserInfo();
+        userInfo.setId(id);
+        userInfo.setStatus(status);
+        baseMapper.updateById(userInfo);
 
     }
 
@@ -64,13 +111,50 @@ public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo> i
 
     @Override
     public UserIndexVO getIndexUserInfo(Long userId) {
-        return null;
+
+        //用户信息
+        UserInfo userInfo = baseMapper.selectById(userId);
+
+        //账户信息
+        QueryWrapper<UserAccount> userAccountQueryWrapper = new QueryWrapper<>();
+        userAccountQueryWrapper.eq("user_id", userId);
+        UserAccount userAccount = userAccountMapper.selectOne(userAccountQueryWrapper);
+
+        //登录日志
+        QueryWrapper<UserLoginRecord> userLoginRecordQueryWrapper = new QueryWrapper<>();
+        userLoginRecordQueryWrapper
+                .eq("user_id",userId)
+                .orderByDesc("id")
+                .last("limit 1");
+        UserLoginRecord userLoginRecord = userLoginRecordMapper.selectOne(userLoginRecordQueryWrapper);
+        //组装结果对象
+        UserIndexVO userIndexVO = new UserIndexVO();
+        userIndexVO.setUserId(userId);
+        userIndexVO.setUserType(userInfo.getUserType());
+        userIndexVO.setName(userInfo.getName());
+        userIndexVO.setNickName(userInfo.getNickName());
+        userIndexVO.setHeadImg(userInfo.getHeadImg());
+        userIndexVO.setBindStatus(userInfo.getBindStatus());
+        userIndexVO.setAmount(userAccount.getAmount());
+        userIndexVO.setFreezeAmount(userAccount.getFreezeAmount());
+        userIndexVO.setLastLoginTime(userLoginRecord.getCreateTime());
+
+        return userIndexVO;
     }
 
     @Override
     public String getMobileByBindCode(String bindCode) {
-        return null;
+
+        QueryWrapper<UserInfo> userInfoQueryWrapper = new QueryWrapper<>();
+        userInfoQueryWrapper
+                .eq("bind_code",bindCode);
+        UserInfo userInfo = baseMapper.selectOne(userInfoQueryWrapper);
+        return userInfo.getMobile();
     }
+
+
+
+
 
     @Transactional(rollbackFor = {Exception.class})
     @Override
