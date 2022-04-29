beb5fe4e6aa7bc6e90f5db5409505b5583e39fe8
suki <15159885771@163.com>
2022-04-29 18:30:10 +0800

    UserBindServiceImpl

--- srm/guigu-service-core/src/main/java/com/atguigu/srb/core/service/impl/UserBindServiceImpl.java
+++ srm/guigu-service-core/src/main/java/com/atguigu/srb/core/service/impl/UserBindServiceImpl.java
@@ -1,12 +1,19 @@
 package com.atguigu.srb.core.service.impl;
 
+import com.atguigu.common.exception.Assert;
+import com.atguigu.common.result.ResponseEnum;
+import com.atguigu.srb.core.enums.UserBindEnum;
+import com.atguigu.srb.core.mapper.UserInfoMapper;
 import com.atguigu.srb.core.pojo.Vo.UserBindVO;
 import com.atguigu.srb.core.pojo.entity.UserBind;
 import com.atguigu.srb.core.mapper.UserBindMapper;
 import com.atguigu.srb.core.service.UserBindService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.Map;
 
 /**
@@ -20,8 +27,36 @@ import java.util.Map;
 @Service
 public class UserBindServiceImpl extends ServiceImpl<UserBindMapper, UserBind> implements UserBindService {
 
+    @Resource
+    private UserInfoMapper userInfoMapper;
     @Override
     public String commitBindUser(UserBindVO userBindVO, Long userId) {
+
+        //不同的user_id, 相同的身份证，如果存在，则不允许
+        QueryWrapper<UserBind> userBindQueryWrapper = new QueryWrapper<>();
+        userBindQueryWrapper
+                .eq("id_card",userBindVO.getIdCard())
+                .ne("user_id",userId);
+        UserBind userBind = baseMapper.selectOne(userBindQueryWrapper);
+
+        Assert.isNull(userBind, ResponseEnum.USER_BIND_IDCARD_EXIST_ERROR);
+
+        //用户是否曾经填写过表单
+        userBindQueryWrapper = new QueryWrapper<>();
+
+        userBindQueryWrapper
+                .eq("user_id",userId);
+
+        if (userBind == null){
+            //创建用户绑定记录
+            userBind = new UserBind();
+            BeanUtils.copyProperties(userBindVO,userBind);
+            userBind.setUserId(userId);
+            userBind.setStatus(UserBindEnum.NO_BIND.getStatus());
+            baseMapper.insert(userBind);
+        }
+
+
         return null;
     }
 
