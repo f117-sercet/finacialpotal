692326b48833a041b7b544e7efb205d644ae535e
suki <15159885771@163.com>
2022-05-09 19:04:49 +0800

    登录接口日志

---
+++ srm/guigu-service-core/src/main/java/com/atguigu/srb/core/controller/api/BorrowerController.java
@@ -0,0 +1,51 @@
+package com.atguigu.srb.core.controller.api;
+
+import com.atguigu.common.result.R;
+import com.atguigu.srb.base.util.JwtUtils;
+import com.atguigu.srb.core.pojo.Vo.BorrowerVO;
+import com.atguigu.srb.core.service.BorrowerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.service.Tags;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Description： 借款人 前端控制器
+ *
+ * @author: 段世超
+ * @aate: Created in 2022/5/9 19:00
+ */
+@Api(tags ="借款人")
+@RestController
+@RequestMapping("/api/core/borrower")
+@Slf4j
+public class BorrowerController {
+
+    @Resource
+    private BorrowerService borrowerService;
+
+    @ApiOperation("保存借款人信息")
+    @PostMapping("/auth/save")
+    public R save(@RequestBody BorrowerVO borrowerVO, HttpServletRequest request) {
+
+        String token = request.getHeader("token");
+        Long userId = JwtUtils.getUserId(token);
+        borrowerService.saveBorrowerVOByUserId(borrowerVO, userId);
+        return R.ok().message("信息提交成功");
+    }
+
+    @ApiOperation("获取借款人认证状态")
+    @GetMapping("/auth/getBorrowerStatus")
+    public R getBorrowerStatus(HttpServletRequest request){
+
+        String token = request.getHeader("token");
+        Long userId = JwtUtils.getUserId(token);
+        Integer status = borrowerService.getStatusByUserId(userId);
+        return R.ok().data("borrowerStatus", status);
+    }
+
+}
