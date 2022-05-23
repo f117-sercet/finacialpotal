3bf29012d2d4e0bce9058c245979bef766f94749
suki <15159885771@163.com>
2022-05-23 18:35:24 +0800

    list.vue

---
+++ srb-admin/src/views/core/borrower/detail.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="app-container">
+  <el-form label-width="100" class="form-table">
+    <el-row>
+      <el-col :span="6">
+        <el-form-item label="状态">
+          {{borrower.status}}
+        </el-form-item>
+      </el-col>
+    </el-row>
+  </el-form>
+</div>
+
+</template>
+<script>
+// 引入组件
+import  borrowerApi from '@/api/core/borrower'
+
+export default {
+
+
+
+}
+
+</script>
