58c5cab2a8ba20107db759f86acc95c266201342
sowrd <15159885771@163.com>
2022-06-07 17:26:46 +0800

    还款计划列表

--- srb-admin/src/views/core/lend/list.vue
+++ srb-admin/src/views/core/lend/list.vue
@@ -17,9 +17,83 @@
       <el-table-column prop="lendStartDate" label="开始日期" />
       <el-table-column prop="lendEndDate" label="结束日期" />
       <el-table-column prop="param.returnMethod" label="还款方式" />
+      <el-table-column prop="param.status" label="状态"/>
 
+      <el-table-column label="操作" width="150" align="center">
+        <template slot-scope="scope">
+         <el-button type="primary" size="mini">
+          <router-link :to="'/core/lend/detail'+scope.row.id">
+           查看
+          </router-link>
+         </el-button>
+         <el-button
+         v-if= "scope.row.status ==1"
+         type="warning"
+         size="mini"
+         @click="makeLoan(scope.row.id)"
+         >放款
+         </el-button>
+        </template>
+      </el-table-column>
 </el-table>
   </div>
 </template>
 <script>
+import lendApi from '@/api/core/lend'
+export default{
+
+data(){
+
+  return{
+    list:null // 列表
+  }
+},
+created(){
+
+this.fetchDate()
+
+},
+methods:{
+
+//加载数据列表
+fetchData(){
+
+  lendApi.getList.then(response =>{
+    this.list = response.data.list
+  })
+},
+
+makeLoan(id){
+  this.$confirm('确定放款吗',"提示",{
+    confirmButtonText:'确定',
+    cancelButtonText:'取消',
+    type:'warning'
+  })
+  .then(()=>{
+
+    return lendApi.makeLoan(id)
+  })
+  .then(response=>{
+    //放款成功，重新获取数据列表
+    this.fetchData()
+    this.$message({
+      type:'success',
+      message:response.message
+    })
+  })
+  .catch(error =>{
+    console.log("取消",error)
+    if(error === "cancel"){
+
+      this.$message({
+      type:'info',
+      message:'已经取消放款'
+      })
+    }
+  })
+}
+
+}
+
+}
 </script>
