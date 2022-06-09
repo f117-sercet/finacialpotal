f18ab4fb069c21e05e4afc9d76f3187601d5d8a9
sowrd <15159885771@163.com>
2022-06-09 18:15:40 +0800

    页面优化

--- srb-admin/src/views/core/borrow-info/list.vue
+++ srb-admin/src/views/core/borrow-info/list.vue
@@ -1,93 +1,94 @@
-<template xmlns:el-table="http://www.w3.org/1999/html">
-<div class="app-container">
-  <!--列表-->
-  <el-table :data="list" stripe>
-    <el-table-column type="index" label="序号" width="60" align="center"/>
-    <el-table-column prop="name" label="借款人姓名" width="90" />
-    <el-table-column prop="mobile" label="手机"/>
-    <el-table-column prop="amount" label="借款金额"/>
-    <el-table-column prop="name" label="借款人姓名" width="90" />
-    <el-table-column  label="借款人姓名">
-      <template slot-scope="scope">{{scope.row.period}}个月</template>
-    </el-table-column>
-    <el-table-column prop="param.returnMethod" label="还款方式"></el-table-column>
-    <el-table-column prop="param.moneyUse" label="资金用途" width="100"></el-table-column>
-    <el-table-column label="年化利率" width="90">
-       <template slot-scope="scope">
-         {{scope.row.borroweYearRate *100}}%
-       </template>
-    </el-table-column>
-    <el-table-column prop="param.status" label="状态" width="100"/>
-    <el-table-colum prop="createTime" label="申请时间" width="150"/>
-    <el-table-colum label="操作" width="150" align="center">
-      <template slot-scope="scope">
-        <el-button type="primary" size="mini">
-          <router-link :to="'/core/borrower/info-detail/' + scope.row.id">
-           查看
-          </router-link>
+<template>
+  <div class="app-container">
+    <!-- 列表 -->
+    <el-table :data="list" stripe>
+      <el-table-column type="index" label="序号" width="60" align="center" />
+      <el-table-column prop="name" label="借款人姓名" width="90" />
+      <el-table-column prop="mobile" label="手机" />
+      <el-table-column prop="amount" label="借款金额" />
+      <el-table-column label="借款期限" width="90">
+        <template slot-scope="scope">{{ scope.row.period }}个月</template>
+      </el-table-column>
+      <el-table-column prop="param.returnMethod" label="还款方式" width="150" />
+      <el-table-column prop="param.moneyUse" label="资金用途" width="100" />
+      <el-table-column label="年化利率" width="90">
+        <template slot-scope="scope">
+          {{ scope.row.borrowYearRate * 100 }}%
+        </template>
+      </el-table-column>
+      <el-table-column prop="param.status" label="状态" width="100" />
+
+      <el-table-column prop="createTime" label="申请时间" width="150" />
+
+      <el-table-column label="操作" width="150" align="center">
+        <template slot-scope="scope">
+          <el-button type="primary" size="mini">
+            <router-link :to="'/core/borrower/info-detail/' + scope.row.id">
+              查看
+            </router-link>
+          </el-button>
+
+          <el-button
+            v-if="scope.row.status === 1"
+            type="warning"
+            size="mini"
+            @click="approvalShow(scope.row)"
+          >
+            审批
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <!-- 审批对话框 -->
+    <el-dialog title="审批" :visible.sync="dialogVisible" width="490px">
+      <el-form label-position="right" label-width="100px">
+        <el-form-item label="是否通过">
+          <el-radio-group v-model="borrowInfoApproval.status">
+            <el-radio :label="2">通过</el-radio>
+            <el-radio :label="-1">不通过</el-radio>
+          </el-radio-group>
+        </el-form-item>
+
+        <el-form-item v-if="borrowInfoApproval.status == 2" label="标的名称">
+          <el-input v-model="borrowInfoApproval.title" />
+        </el-form-item>
+
+        <el-form-item v-if="borrowInfoApproval.status == 2" label="起息日">
+          <el-date-picker
+            v-model="borrowInfoApproval.lendStartDate"
+            type="date"
+            placeholder="选择开始时间"
+            value-format="yyyy-MM-dd"
+          />
+        </el-form-item>
+
+        <el-form-item v-if="borrowInfoApproval.status == 2" label="年化收益率">
+          <el-input v-model="borrowInfoApproval.lendYearRate">
+            <template slot="append">%</template>
+          </el-input>
+        </el-form-item>
+
+        <el-form-item v-if="borrowInfoApproval.status == 2" label="服务费率">
+          <el-input v-model="borrowInfoApproval.serviceRate">
+            <template slot="append">%</template>
+          </el-input>
+        </el-form-item>
+
+        <el-form-item v-if="borrowInfoApproval.status == 2" label="标的描述">
+          <el-input v-model="borrowInfoApproval.lendInfo" type="textarea" />
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">
+          取消
         </el-button>
-
-        <el-button
-        v-if="scope.row.status===1"
-        type="warning"
-        size="mini"
-        @click="approvalShow(scope.row)"
-        >审批
+        <el-button type="primary" @click="approvalSubmit">
+          确定
         </el-button>
-
-      </template>
-    </el-table-colum>
-  </el-table>
-
-  <!-- 审批对话框 -->
-  <el-dialog title="审批" :visible.sync="dialogVisible" width="490px">
-    <el-form label-position="right" label-width="100px">
-      <el-form-item label="是否通过">
-        <el-radio-group v-model="borrowInfoApproval.status">
-          <el-radio :label="2">通过</el-radio>
-          <el-radio :label="-1">不通过</el-radio>
-        </el-radio-group>
-      </el-form-item>
-
-      <el-form-item v-if="borrowInfoApproval.status == 2" label="标的名称">
-        <el-input v-model="borrowInfoApproval.title" />
-      </el-form-item>
-
-      <el-form-item v-if="borrowInfoApproval.status == 2" label="起息日">
-        <el-date-picker
-          v-model="borrowInfoApproval.lendStartDate"
-          type="date"
-          placeholder="选择开始时间"
-          value-format="yyyy-MM-dd"
-        />
-      </el-form-item>
-
-      <el-form-item v-if="borrowInfoApproval.status == 2" label="年化收益率">
-        <el-input v-model="borrowInfoApproval.lendYearRate">
-          <template slot="append">%</template>
-        </el-input>
-      </el-form-item>
-
-      <el-form-item v-if="borrowInfoApproval.status == 2" label="服务费率">
-        <el-input v-model="borrowInfoApproval.serviceRate">
-          <template slot="append">%</template>
-        </el-input>
-      </el-form-item>
-
-      <el-form-item v-if="borrowInfoApproval.status == 2" label="标的描述">
-        <el-input v-model="borrowInfoApproval.lendInfo" type="textarea" />
-      </el-form-item>
-    </el-form>
-    <div slot="footer" class="dialog-footer">
-      <el-button @click="dialogVisible = false">
-        取消
-      </el-button>
-      <el-button type="primary" @click="approvalSubmit">
-        确定
-      </el-button>
-    </div>
-  </el-dialog>
-</div>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
