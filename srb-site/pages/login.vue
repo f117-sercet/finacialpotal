937fcca47253f4c4ff3d9f093b97ae44e36b8d68
sowrd <15159885771@163.com>
2022-06-10 17:56:49 +0800

    前端页面展示

---
+++ srb-site/pages/login.vue
@@ -0,0 +1,14 @@
+<template>
+    
+</template>
+
+<script>
+import '~/assets/css/register.css'
+import cookie from 'js-cookie'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
