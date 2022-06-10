12958e45907d6a26940e1bf2577405f9ff481ad3
sowrd <15159885771@163.com>
2022-06-10 18:23:18 +0800

    还是前端香啊

---
+++ srb-site/pages/about/detail.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="text-box">
+<div class="text-conter" id="text-content">
+    <header class="article-title">
+        <div class="article-title"><a href="#">关爱社区环卫工再行动</a></div>
+        <div class="article-meta">
+            <span class="item">2020-09-01</span>
+            
+        </div>
+    </header>
+</div>
+</div>
+</template>
