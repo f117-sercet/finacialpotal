102e59b08560d002b3c3bc29137290933e0af84b
sowrd <15159885771@163.com>
2022-06-16 14:36:01 +0800

    bind.vue页面开发完毕

--- srb-site/pages/user/bind.vue
+++ srb-site/pages/user/bind.vue
@@ -1,6 +1,83 @@
 <template>
-<div>
-
+<div class="personal-main">
+<div class="personal-pay">
+    <h3><i> 开通第三方账户</i></h3>
+</div>
+<div class="pay-notice">
+<p>
+    请开通汇付宝存管账户以便于您正常理财
+</p>
+</div>
+<div class="pay-form">
+<ul>
+    <li>
+    <label>真实姓名</label>
+    <input 
+    v-model="userBind.name"
+    type="text"
+    class="pay-txt"
+    maxlength="16"
+    placeholder="您的真实姓名"
+    />
+    </li>
+    <li>
+        <label>身份证号</label>
+        <input 
+         v-model="userBind.idCard"
+         type="text"
+         class="pay-txt"
+         maxlength="18"
+         placeholder="您的身份证号"
+        />
+        <div id="idCardErrorDiv">
+            <p style="margin-top: 10px;">
+             身份证信息认证后将不可修改，请您仔细填写
+            </p>
+        </div>
+    </li>
+    <li>
+        <label>绑定银行</label>
+        <input
+        v-model="userBind.bankType"
+        type="text"
+        class="pay-txt"
+        placeholder="银行名称"
+        />
+    </li>
+    <li>
+        <label>银行卡号</label>
+        <input 
+           v-model="userBind.bankNo"
+           type="text"
+           class="pay-txt"
+           placeholder="本人持有的银行卡"
+        />
+    </li>
+    <li>
+        <label>预预留手机</label>
+        <input 
+        v-model="userBind.mobile"
+        type="text"
+        class="pay-txt"
+        placeholder="银行预留卡号"
+        />
+    </li>
+    <li>
+        <label>>&nbsp;</label>
+        <input v-model="agree" type="checkbox" />
+        我已经阅读并同意
+        <a href="#" class="c-orange" target="_blank">
+            《汇付宝托管账户协议》
+        </a>
+    </li>
+    <li>
+        <label>&nbsp;</label>
+        <el-button :disabled="!agree" @click="commitBind()" type="primary">
+            开户
+        </el-button>
+    </li>
+</ul>
+</div>
 </div>
 </template>
 
@@ -8,7 +85,6 @@
 export default {
 
 data(){
-
     return{
 
         agree:false,
